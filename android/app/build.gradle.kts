3e82aa9dc6b12b95ed1100a2a8405d787e06df3d
Raafa19 <rafamsuarez19@gmail.com>
2025-07-21 18:58:59 -0400

    cambio paquete 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.rafamsuarez.music_player_2"
+        applicationId = "com.rafamsuarez.music_player"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
